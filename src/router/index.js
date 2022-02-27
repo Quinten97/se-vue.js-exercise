219bc352af2ebe90dae126588475266e13adefd6
Quinten97 <89324378+Quinten97@users.noreply.github.com>
2022-02-27 00:51:02 -0500

    changed site title and favicon.ico

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    components: { default: Home}
+    components: { default: Home},
+    meta: {
+      title: "Home"
+    }
   },
   {
     path: '/about',
